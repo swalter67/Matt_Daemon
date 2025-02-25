8b048035d6637f0c00e61152183043af36ce9189
lomasson <loic.masson67@hotmail.fr>
2025-02-25 13:59:03 +0100

    feat: multiple Ben client at same time

--- benafk/BenAFK.cpp
+++ benafk/BenAFK.cpp
@@ -1,11 +1,10 @@
 #include "BenAFK.hpp"
 
-
 void BenAFK::send_pubkey(void)
 {
-    char   *key_sended;
-    int     key_len;
-    
+    char *key_sended;
+    int key_len;
+
     BIO *pub = BIO_new(BIO_s_mem());
     PEM_write_bio_RSAPublicKey(pub, this->keys);
     key_len = BIO_pending(pub) + 1;
@@ -15,66 +14,69 @@ void BenAFK::send_pubkey(void)
     // #ifdef PRINT_KEYS
     //     printf("\nben key: %s\n", key_sended);
     // #endif
-    std::string sended(FIRST_MESSAGE);
+    std::string sended(BEN_MSG);
     sended.append(key_sended);
     send(this->sock, sended.c_str(), sended.size(), 0);
+    printf("key has been send\n");
     free(key_sended);
     BIO_free(pub);
 }
 
 void BenAFK::get_pubkey()
 {
-    char   key_readed[KEY_LENGTH];
-    
+    char key_readed[KEY_LENGTH];
+
     memset(key_readed, 0, KEY_LENGTH);
     int readoc = recv(this->sock, key_readed, KEY_LENGTH, 0);
-    BIO* bio = BIO_new_mem_buf((void*)key_readed, readoc);
+    BIO *bio = BIO_new_mem_buf((void *)key_readed, readoc);
     this->pub_matt = PEM_read_bio_RSAPublicKey(bio, NULL, NULL, NULL);
     BIO_free(bio);
     // #ifdef PRINT_KEYS
-        // BIO *pub2 = BIO_new(BIO_s_mem());
-        // PEM_write_bio_RSAPublicKey(pub2, this->pub_matt);
-        // int key_len = BIO_pending(pub2);
-        // char *pub_key = (char *)calloc(key_len + 1, 1);
-        // BIO_read(pub2, pub_key, key_len);
+    // BIO *pub2 = BIO_new(BIO_s_mem());
+    // PEM_write_bio_RSAPublicKey(pub2, this->pub_matt);
+    // int key_len = BIO_pending(pub2);
+    // char *pub_key = (char *)calloc(key_len + 1, 1);
+    // BIO_read(pub2, pub_key, key_len);
 
-        // pub_key[key_len] = '\0';
-        // printf("len :%d [%s]", key_len, pub_key);
+    // pub_key[key_len] = '\0';
+    // printf("len :%d [%s]", key_len, pub_key);
     // #endif
 }
 
 int BenAFK::encrypt_message(const char *from, int fromlen, char *to)
 {
-    int len = RSA_public_encrypt(fromlen, (unsigned char*)from, (unsigned char*)to, this->pub_matt, RSA_PKCS1_OAEP_PADDING);
-    if (len < 0 )
+    int len = RSA_public_encrypt(fromlen, (unsigned char *)from, (unsigned char *)to, this->pub_matt, RSA_PKCS1_OAEP_PADDING);
+    if (len < 0)
         perror("encryption");
     return len;
 }
 
-
-void BenAFK::decrypt_message(char* from , int fromlen, char* to)
+void BenAFK::decrypt_message(char *from, int fromlen, char *to)
 {
-    if (RSA_private_decrypt(fromlen, (unsigned char*)from, (unsigned char*)to, this->keys, RSA_PKCS1_OAEP_PADDING) < 0 )
+    if (RSA_private_decrypt(fromlen, (unsigned char *)from, (unsigned char *)to, this->keys, RSA_PKCS1_OAEP_PADDING) < 0)
         perror("decryption");
 }
 
 BenAFK::BenAFK()
 {
     this->pub_matt = NULL;
+    int optval = 1;
     this->keys = RSA_generate_key(KEY_LENGTH, PUB_EXP, NULL, NULL);
 
     this->sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (this->sock == -1) {
+    if (this->sock == -1)
+    {
         perror("socket creation");
         exit(EXIT_FAILURE);
     }
-    setsockopt(this->sock, SOL_SOCKET, SO_REUSEADDR, &(int){1}, sizeof(int));
+    setsockopt(this->sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
     sockaddr_in server_addr;
     memset(&server_addr, 0, sizeof(sockaddr_in));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
-    if (connect(this->sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+    if (connect(this->sock, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
         perror("binding socket");
         exit(EXIT_FAILURE);
     }
@@ -87,16 +89,19 @@ BenAFK::BenAFK()
     }
 }
 
-BenAFK::BenAFK(const BenAFK& src) {
+BenAFK::BenAFK(const BenAFK &src)
+{
     this->sock = src.sock;
 }
 
-BenAFK& BenAFK::operator=(const BenAFK& src) {
+BenAFK &BenAFK::operator=(const BenAFK &src)
+{
     this->sock = src.sock;
     return (*this);
 }
 
-BenAFK::~BenAFK(){
+BenAFK::~BenAFK()
+{
     close(sock);
 }
 
--- mattdaemon/MattDaemon.hpp
+++ mattdaemon/MattDaemon.hpp
@@ -10,13 +10,13 @@
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
-#include <ctime> 
+#include <ctime>
 #include <signal.h>
-// #include <openssl/evp.h>
-// #include <openssl/pem.h>
-// #include <openssl/types.h>
 #include <openssl/rsa.h>
 #include <openssl/engine.h>
+
+#include <iostream>
+#include <fstream>
 #include "Tintin_reporter.hpp"
 #include <map>
 #define LOCK_FILE "/var/lock/matt_daemon.lock"
@@ -25,38 +25,41 @@
 #define PORT 4242
 
 #define KEY_LENGTH  2048
-#define PUB_EXP     3
-#define PRINT_KEYS
-#define FIRST_MESSAGE "!ben"
+#define PUB_EXP 3
+// #define LOG
+#define BEN_MSG "!ben"
 #define MAX_CLIENT 3
 #define BUFFER_SIZE 1024
 
+class MattDaemon
+{
+    public:
+        MattDaemon();
+        ~MattDaemon();
+        void run();
 
-class MattDaemon {
-public:
-    MattDaemon();
-    ~MattDaemon();
-    void run();
+    private:
+        void daemonize();
+        void setupLockFile();
+        void setupServer();
 
-private:
-    void daemonize();
-    void setupLockFile();
-    void setupServer();
-    void get_pub_key_from_client(int sock, char *buffer, int bsize);
-    RSA *get_keys_by_sock(int sock);
-    int encrypt_message(const char *message, char *buffer, RSA *pubkey);
-    void decrypt_message(char* from , int fromlen, char* to);
-    int new_connection(fd_set *active_fd_set);
-    void send_pubkey(int sock);
-    ///void signalHandler(int signum);
-    int lock_fd;
-    int server_fd;
-    int max_fd;
-    int client_count; // Nombre de clients connectés
-    Tintin_reporter logger;
-    std::ofstream myfile;
-    RSA         *keys;
-    std::map<int, RSA *> *clients_keys;
+        void get_pub_key_from_client(int sock, char *buffer, int bsize);
+        RSA *get_keys_by_sock(int sock);
+        int encrypt_message(const char *message, char *buffer, RSA *pubkey);
+        void decrypt_message(char *from, int fromlen, char *to);
+        int new_connection(fd_set *active_fd_set);
+        void send_pubkey(int sock);
+        /// void signalHandler(int signum);
+        int lock_fd;
+        int server_fd;
+        int max_fd;
+        int client_count; // Nombre de clients connectés
+        Tintin_reporter logger;
+        #ifdef LOG
+            std::ofstream      myfile;
+        #endif
+        RSA *keys;
+        std::map<int, RSA *> *clients_keys;
 };
 
 #endif // MATTDAEMON_HPP
